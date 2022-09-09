f3f334edc948d1ed593cbc9536c93d036a214377
alberth04 <godoyalberth.2002@gmail.com>
2022-09-09 13:07:24 -0600

    Empezando Laboratorio Lab 8

---
+++ src/lab8p2_alberthgodoy/Main.java
@@ -0,0 +1,16 @@
+package lab8p2_alberthgodoy;
+
+/**
+ *
+ * @author godoy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
